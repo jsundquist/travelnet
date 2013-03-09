5f0a6594f775d4ac1507d75bd0529aa3d18a8ad4
jsundquist <jsundquist@gmail.com>
2013-03-08 19:48:36 -0600

    Adding in the layout information

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,17 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    /**
+     * Initialize the twillio service so we can use it within the rest of the application
+     *
+     * @return Services_Twilio
+     */
+    protected function _initTwilio()
+    {
+        $config = $this->getOption('twilio');
+        $twilio = new Services_Twilio($config['sid'], $config['token']);
+        return $twilio;
+    }
 
 }
 
