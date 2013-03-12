d9df68b5a241e9085ee14e7210b010072ccdad05
jsundquist <jsundquist@gmail.com>
2013-03-11 20:43:25 -0500

    Updated to allow creating, editing, and deleting of contacts.  Also fixed a few links within the properties views.

---
+++ application/forms/Contact.php
@@ -0,0 +1,54 @@
+<?php
+class Application_Form_Contact extends Zend_Form {
+
+    public function init(){
+
+        $this->setMethod("post");
+
+        $this->addElement('hidden', 'id', array(
+            'ignore' => true,
+            'filters' => array('Int')
+        ));
+
+        $this->addElement('text','name', array(
+            'label' => 'Name',
+            'required' => true,
+            'filters' => array('StringTrim')
+        ));
+
+        $this->addElement('text','address', array(
+            'label' => 'Address',
+            'required' => true,
+            'filters' => array('StringTrim')
+        ));
+
+        $this->addElement('text','city', array(
+            'label' => 'City',
+            'required' => true,
+            'filters' => array('StringTrim')
+        ));
+
+        $this->addElement('text','state', array(
+            'label' => 'State',
+            'required' => true,
+            'filters' => array('StringTrim')
+        ));
+
+        $this->addElement('text','postal_code', array(
+            'label' => 'Postal Code',
+            'required' => true,
+            'filters' => array('StringTrim')
+        ));
+
+        $this->addElement('text','phone_number', array(
+            'label' => 'Phone Number',
+            'required' => true,
+            'filters' => array('StringTrim')
+        ));
+
+        $this->addElement('submit','submit', array(
+            'ignore' => true,
+            'label' => 'Submit Contact'
+        ));
+    }
+}
