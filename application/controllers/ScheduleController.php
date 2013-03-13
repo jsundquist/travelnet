dae6931c8c0200f055a1ec5bf725cc35171dc77e
jsundquist <jsundquist@gmail.com>
2013-03-12 19:24:58 -0500

    Updated so we can make some test phone calls.

---
+++ application/controllers/ScheduleController.php
@@ -0,0 +1,49 @@
+<?php
+
+class ScheduleController extends Zend_Controller_Action
+{
+
+    /**
+     * Display a list of all the scheduled calls. We should only see the calls that have not yet been made
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Add a call to our database so it can be sent off to twilio
+     */
+    public function add()
+    {
+        $AccountSid = 'AC6a91408d4993b7d6bee3efa15d19f80d';
+        $AuthToken = 'd861f6128265464d0830f8b8bb28a366';
+
+        $client = new Services_Twilio($AccountSid, $AuthToken);
+        $call = $client->account->calls->create('6123548270', '6122708838', 'http://travelnet.digital-portfolio.net/call');
+    }
+
+    /**
+     * Edit a call.  This will only be allowed if twilio has not yet made the call.
+     */
+    public function edit()
+    {
+
+    }
+
+    /**
+     * View the information about the call
+     */
+    public function view()
+    {
+
+    }
+
+    /**
+     * Delete the call. Only allowed if the call has not yet been placed by twilio.
+     */
+    public function delete()
+    {
+
+    }
+}
